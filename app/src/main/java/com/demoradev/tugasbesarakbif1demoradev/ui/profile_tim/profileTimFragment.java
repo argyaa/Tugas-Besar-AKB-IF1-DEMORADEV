d20920ccd60b4afb4668c04548d215a0a351250a
Gissart <argyaauliaf@gmail.com>
2022-07-30 16:34:53 +0700

    add profile team fragment

--- app/src/main/java/com/demoradev/tugasbesarakbif1demoradev/ui/profile_tim/profileTimFragment.java
+++ app/src/main/java/com/demoradev/tugasbesarakbif1demoradev/ui/profile_tim/profileTimFragment.java
@@ -7,15 +7,40 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.demoradev.tugasbesarakbif1demoradev.R;
+import com.demoradev.tugasbesarakbif1demoradev.models.teamModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class profileTimFragment extends Fragment {
-
+    RecyclerView recyclerView;
+    profileTimAdapter adapter;
+    List<teamModel> teamList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile_tim, container, false);
+        recyclerView = view.findViewById(R.id.profile_tim_recyclerView);
+
+        teamList = new ArrayList<>();
+
+        int[] image = {R.drawable.ivan_akb, R.drawable.fiona_akb, R.drawable.tommy_akb, R.drawable.argya_akb};
+        String[] nama = {"Ivan Faathirza", "Fiona Avila Putri", "Muhammad Khatami", "Argya Aulia Fauzandika"};
+        String[] nim = {"10119003", "10119013", "10119026", "10119035"};
+
+        for (int i=0; i< image.length; i++){
+            teamModel item = new teamModel(image[i], nama[i], nim[i]);
+            teamList.add(item);
+        }
+
+        adapter = new profileTimAdapter(getActivity(), teamList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         return view;
     }
 
