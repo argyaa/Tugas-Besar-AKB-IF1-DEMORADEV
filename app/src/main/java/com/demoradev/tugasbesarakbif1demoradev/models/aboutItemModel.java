5401df06477365fe7f4328477a4135c4c9d32810
Gissart <argyaauliaf@gmail.com>
2022-07-30 14:28:25 +0700

    add about item model

---
+++ app/src/main/java/com/demoradev/tugasbesarakbif1demoradev/models/aboutItemModel.java
@@ -0,0 +1,10 @@
+package com.demoradev.tugasbesarakbif1demoradev.models;
+
+public class aboutItemModel {
+    public String title, description;
+
+    public aboutItemModel(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
