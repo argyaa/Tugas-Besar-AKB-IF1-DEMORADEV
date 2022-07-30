59a8001a7900dd94c217bde7d383f4d2d2c5f647
Gissart <argyaauliaf@gmail.com>
2022-07-30 14:24:38 +0700

    add team model

---
+++ app/src/main/java/com/demoradev/tugasbesarakbif1demoradev/models/teamModel.java
@@ -0,0 +1,12 @@
+package com.demoradev.tugasbesarakbif1demoradev.models;
+
+public class teamModel {
+    public int image;
+    public String nama, nim;
+
+    public teamModel(int image, String nama, String nim) {
+        this.image = image;
+        this.nama = nama;
+        this.nim = nim;
+    }
+}
