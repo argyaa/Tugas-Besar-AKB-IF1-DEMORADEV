795b5d1b0ee12381690517bbbf1efe8fff70b9ae
Gissart <argyaauliaf@gmail.com>
2022-07-30 14:18:22 +0700

    add team model

---
+++ app/src/main/java/com/demoradev/tugasbesarakbif1demoradev/models/teamModel.java
@@ -0,0 +1,19 @@
+/*
+ *   NIM : 10119035
+ *   NAMA : ARGYA AULIA FAUZANDIKA
+ *   KELAS : IF-1
+ * */
+
+package com.demoradev.tugasbesarakbif1demoradev.models;
+
+
+public class teamModel {
+    public int image;
+    public String title, desc;
+
+    public teamModel(int image, String title, String desc) {
+        this.image = image;
+        this.title = title;
+        this.desc = desc;
+    }
+}
