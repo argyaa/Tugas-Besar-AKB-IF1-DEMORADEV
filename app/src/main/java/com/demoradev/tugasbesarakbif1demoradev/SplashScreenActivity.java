9f2b9847081b8db22fc8835dc5a5858da7a3c485
alfiyandipandya03 <alfiyandipandya03@gmail.com>
2022-07-31 13:47:56 +0700

    Update SplashScreenActivity.java

--- app/src/main/java/com/demoradev/tugasbesarakbif1demoradev/SplashScreenActivity.java
+++ app/src/main/java/com/demoradev/tugasbesarakbif1demoradev/SplashScreenActivity.java
@@ -1,14 +1,35 @@
 package com.demoradev.tugasbesarakbif1demoradev;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+//
+// Nama  : Alfiyandi Pandya K
+// Nim   : 10119007
+// Kelas : IF 1
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
     }
 }
